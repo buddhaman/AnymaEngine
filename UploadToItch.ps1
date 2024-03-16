6514fce1bbfc6d513dce00daaa4dbaecc2614ae0
Tim Trussner <tim.trussner@gmail.com>
2024-03-16 12:41:35 +0100

    New release script in powershell :(((

---
+++ UploadToItch.ps1
@@ -0,0 +1,27 @@
+# Define variables
+$ProjectName = "AnymaEngine"
+$ItchUser = "buddhaman"
+$ItchGame = "anyma-engine"
+$Channel = "win64-release"
+$BuildDir = "build/Release"
+$ZipName = "build/${ProjectName}_${Channel}.zip"
+
+# Change to the script's directory
+Set-Location -Path $PSScriptRoot
+
+# Zip the build directory
+Write-Host "Zipping the build directory..."
+try {
+    Compress-Archive -Path "${BuildDir}\*" -DestinationPath $ZipName -Force -ErrorAction Stop
+}
+catch {
+    Write-Error "Zipping failed: $_"
+    exit 1
+}
+
+# Upload the zip file to itch.io using butler
+Write-Host "Uploading the build to itch.io..."
+$butlerPushCommand = "butler push $($ZipName) $($ItchUser)/$($ItchGame):$($Channel)"
+Invoke-Expression $butlerPushCommand
+
+Write-Host "Upload complete!"
