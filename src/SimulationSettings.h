bcb2d1bbeae392d118358b61221add13d6ac7d3a
Tim Trussner <tim.trussner@gmail.com>
2024-04-06 15:33:14 +0200

    Made debug settings

--- src/SimulationSettings.h
+++ src/SimulationSettings.h
@@ -2,12 +2,18 @@
 
 #include "AnymUtil.h"
 
+#ifdef NDEBUG
+#define DEFAULT_AXIS_CHUNKS 200
+#else
+#define DEFAULT_AXIS_CHUNKS 60
+#endif
+
 struct SimulationSettings
 {
     int max_agents = 5000;
     int n_initial_agents = 1000;
     R32 chunk_size = 12.0f;
-    int x_chunks = 60;
-    int y_chunks = 60;
+    int x_chunks = DEFAULT_AXIS_CHUNKS;
+    int y_chunks = DEFAULT_AXIS_CHUNKS;
     R32 default_mutation_rate = 0.03f;
 };
